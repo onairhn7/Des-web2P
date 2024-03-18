90e64baf9078e8204ff5c65cf532921c67ffa352
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2024-03-18 15:07:07 -0600

    conectando con front

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import Express  from "express";
 const app= Express();
 import { usuario } from "./routes/routeUser.js";
+import cors from 'cors';
 
 
 //middleware
 
 app.use(Express.json());
+const corOptions = {
+    origin : '10.0.0.130', 
+    methods : ['POST', 'PUT', 'DELETE', 'GET'],
+    allowedHeaders : ['Content-Type', 'Authorization']
+}
 
+// Rutas
 app.use('/api/usuario', usuario);
 
-
+// puerto
 app.listen(4500, ()=>{
 
     console.log("escuchando en el puerto 4500");
